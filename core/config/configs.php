603d597ee369fec0a5d6c22a07a6c56f1718cf39
arash691 <arash.sw90@yahoo.com>
2014-10-08 22:40:55 +0330

    Create configs.php

---
+++ core/config/configs.php
@@ -0,0 +1,6 @@
+<?php
+$configs = array();
+$configs['baseURL'] = 'localhost/mvc/';
+$configs['defaultController'] = 'Site';
+$configs['defaultAction'] = 'index';
+$configs['invalidURLChars'] = '#[^a-z0-9\-_\/]#i';
