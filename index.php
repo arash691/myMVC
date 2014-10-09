4833c9cfb483f302e74aba28464f0c80b59c089f
Arash Aryani <arash.sw90@yahoo.com>
2014-10-09 21:00:28 +0330

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+require_once 'core/init.php';
+require_once 'core/classloader.php';
+
+try{
+    Initializer::init();
+    $router = Loader::load('Router');
+    Dispatcher::dispatch($router);
+}catch (Exception $exc){
+     echo '<p style="background-color: darkorange;color: antiquewhite;width: 400px;height: 25px;padding: 20px;text-align: center">'.$exc->getMessage().'</p>';
+}
