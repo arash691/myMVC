fe41b830a2b92c6d8f274eefe1b386e51dc4ed5a
Arash Aryani <arash.sw90@yahoo.com>
2014-10-10 11:38:29 +0330

    test2

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ try{
     $router = Loader::load('Router');
     Dispatcher::dispatch($router);
     echo 'test';
+    echo 'hi';
 }catch (Exception $exc){
      echo '<p style="background-color: darkorange;color: antiquewhite;width: 400px;height: 25px;padding: 20px;text-align: center">'.$exc->getMessage().'</p>';
 }
